df321053d3c6ffdfe3f234ac7346510abd46c477
fulanooo <114761708+fulanooo@users.noreply.github.com>
2022-09-30 13:32:22 -0300

    upload

---
+++ blog_coder/blog/migrations/0002_alter_posts_body.py
@@ -0,0 +1,18 @@
+
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='posts',
+            name='body',
+            field=models.CharField(max_length=1000),
+        ),
+    ]
